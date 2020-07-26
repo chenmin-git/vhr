0b9cdf9a2909cec74174c2e9681153db4c5f0e77
Administrator <2857135639@qq.com>
2020-07-26 21:13:03 +0800

    shanima

---
+++ src/main/java/cn/jbit/vhr/config/MySecurityConfig.java
@@ -0,0 +1,21 @@
+package cn.jbit.vhr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+}
---
+++ src/main/java/cn/jbit/vhr/controller/TestController.java
@@ -0,0 +1,17 @@
+package cn.jbit.vhr.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
+public class TestController {
+    @GetMapping("/test")
+    public String getTest() {
+        System.out.println("shanima");
+        return JSON.toJSONString("Stringjsdkaljfklsdjfkdslf");
+    }
+
+}
